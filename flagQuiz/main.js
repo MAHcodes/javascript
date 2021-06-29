7e7689ce1721207d174f477ee0355cc76d5213e6
mhmdali102 <mhmdali102@bk.ru>
2021-06-29 08:59:38 +0300

    share points

--- flagQuiz/main.js
+++ flagQuiz/main.js
@@ -5,16 +5,25 @@ const fireBtn = document.getElementById("fire");
 const score = document.getElementById("score");
 const hintBtn = document.getElementById("hint");
 const skipBtn = document.getElementById("skip");
+const fbBtn = document.getElementById("share-facebook");
+const wpBtn = document.getElementById("share-whatsapp");
+const twBtn = document.getElementById("share-twitter");
 
 let allCountries = null;
 let theCountry = null;
 let scoreValue = 5;
 let ifNewCountry = true;
+let ifWP = true;
+let ifFB = true;
+let ifTW =  true;
 
 window.addEventListener("load", restoreLocal);
 fireBtn.addEventListener("click", checkInput);
 hintBtn.addEventListener("click", getHint);
 skipBtn.addEventListener("click", skipFlag);
+wpBtn.addEventListener("click", shareWhatsapp);
+fbBtn.addEventListener("click", shareFacebook);
+twBtn.addEventListener("click", shareTwitter);
 user.addEventListener("keypress", (e) => {
     if (e.keyCode === 13 && e.key == "Enter") {
         fireBtn.click();
@@ -56,7 +65,7 @@ function skipFlag() {
 function getHint() {
     if (scoreValue >= 1) {
         if (! ifNewCountry == false) {
-            if (!confirm("Get Hint For 1 Points?")) {return;}
+            if (!confirm("Get Hint For 1 Point?")) {return;}
             scoreValue--;
             score.innerText = scoreValue;
             ifNewCountry = false;
@@ -81,22 +90,67 @@ function checkInput() {
 
 function win() {
     scoreValue++;
-score.innerText = scoreValue;
+    score.innerText = scoreValue;
     ifNewCountry = true;
     generateRandomFlag();
     save2Local();
+    updateUrl();
 };
 
 function save2Local() {
     localStorage.setItem("flagScore", scoreValue);
 };
 
+function saveShares() {
+    localStorage.setItem("flagShares", [ifFB, ifWP, ifTW]);
+};
+
 function restoreLocal() {
     if (localStorage.getItem("flagScore") == null) {
         scoreValue = 5;
     } else {
-        score.innerText = localStorage.getItem("flagScore");
+        scoreValue = localStorage.getItem("flagScore");
+        score.innerText = scoreValue;
+        updateUrl();
+    };
+    if (localStorage.getItem("flagShares") == null) {
+        flagShares = [true, true, true];
+    } else {
+        let flagShares = localStorage.getItem("flagShares").split(",");
+        ifFB = flagShares[0];
+        ifWP = flagShares[1];
+        ifTW = flagShares[2];
+    }; 
+};
+
+function shareWhatsapp() {
+    checkIfOnce(ifWP);
+    ifWP = false;
+    saveShares();
+};
+
+function shareFacebook() {
+    checkIfOnce(ifFB);
+    ifFB = false;
+    saveShares();
+};
+
+function shareTwitter() {
+    checkIfOnce(ifTW);
+    ifTW = false;
+    saveShares();
+};
+
+function checkIfOnce(e) {
+    if (e == true || e == "true") {
+        scoreValue = parseInt(scoreValue, 10) + 4;
+        score.innerText= scoreValue;
+        save2Local();
     };
 };
 
-console.log(document.querySelector(".share"))
+function updateUrl() {
+    wpBtn.href = `whatsapp://send?text=I got ${scoreValue} point(s) in the Flag Quiz Game. Try to beat me! https://bit.ly/3A8yQxd`;
+    fbBtn.href = `https://www.facebook.com/share.php?u=https://bit.ly/3A8yQxd&quote=I got ${scoreValue} point(s) in the Flag Quiz Game. Try to beat me! https://bit.ly/3A8yQxd`;
+    twBtn.href = `https://twitter.com/share?text=I got ${scoreValue} point(s) in the Flag Quiz Game. Try to beat me! https://bit.ly/3A8yQxd`;
+};
